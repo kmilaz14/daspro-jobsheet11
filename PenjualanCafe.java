5c11b9fd5b5c621fefd679f88b5937e8dfedfd70
kmilaz14 <kamilazhwa@gmail.com>
2024-11-21 14:23:18 +0800

    revisi tugas 2

--- PenjualanCafe.java
+++ PenjualanCafe.java
@@ -12,7 +12,7 @@ public class PenjualanCafe {
 
     static String[] menu = {"Kopi", "Teh", "Es Degan", "Roti Bakar", "Gorengan"};
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean program = true;
 
